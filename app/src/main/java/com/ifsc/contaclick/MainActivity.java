d9ba46a205ccf02f2d6f63525571f5ea788e8ce3
joaoP2006 <joao.pv09@aluno.ifsc.edu.br>
2025-03-31 08:16:18 -0300

    ciclo de vida

--- app/src/main/java/com/ifsc/contaclick/MainActivity.java
+++ app/src/main/java/com/ifsc/contaclick/MainActivity.java
@@ -1,12 +1,15 @@
 package com.ifsc.contaclick;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+        Log.d("ciclo de vida", "mensagem");
         setContentView(R.layout.activity_main);
         edpeso=findViewById(R.id.edpeso);
         edaltura=findViewById(R.id.edaltura);
@@ -30,11 +34,41 @@ public class MainActivity extends AppCompatActivity {
               peso=Double.parseDouble(edpeso.getText().toString());
               altura=Double.parseDouble(edaltura.getText().toString());
               imc=peso/(altura*altura);
+              //formatando numero ##,##
+              DecimalFormat decimalFormat=new DecimalFormat("##.##");
+
 
               tvresultado.setText(Double.toString(imc));});
 
 
 
+    }
+
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("ciclo de vida", "mensagem");
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("ciclo de vida", "mensagem");
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("ciclo de vida", "mensagem");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("ciclo de vida", "mensagem");
     }
 }
 
