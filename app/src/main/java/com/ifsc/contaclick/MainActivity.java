43c943fc85e395f5c763f5499d9f1d5b99b33131
joaoP2006 <joao.pv09@aluno.ifsc.edu.br>
2025-04-14 09:01:35 -0300

    modificaçoes no codigo imc

--- app/src/main/java/com/ifsc/contaclick/MainActivity.java
+++ app/src/main/java/com/ifsc/contaclick/MainActivity.java
@@ -1,54 +1,45 @@
 package com.ifsc.contaclick;
 
+import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
 
-    int i = 0;
-    EditText edpeso, edaltura;
-    TextView tvresultadoimc;
-    Button Button;
+    EditText edtPeso, edtAltura;
+    Button btnCalcular;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d("ciclo de vida", "metodo onCreate");
         setContentView(R.layout.activity_main);
-        edpeso= findViewById(R.id.edpeso);
-        edaltura= findViewById(R.id.edaltura);
-        tvresultadoimc= findViewById(R.id.tvresultadoimc);
-        Button= findViewById(R.id.button);
 
-        Button.setOnClickListener(v -> {
-            Intent intent = new Intent(getApplicationContext(), MainActivityB.class);
-            Double peso = Double.parseDouble(edpeso.getText().toString());
-            Double altura = Double.parseDouble(edaltura.getText().toString());
-            double resultado = peso * (altura*altura);
+        edtPeso = findViewById(R.id.edtPeso);
+        edtAltura = findViewById(R.id.edtAltura);
+        btnCalcular = findViewById(R.id.btnCalcular);
 
-            String msg = edpeso.getText().toString();
-            intent.putExtra("mensagem", msg);
-            startActivity(intent);
+        btnCalcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String pesoStr = edtPeso.getText().toString();
+                String alturaStr = edtAltura.getText().toString();
 
+                if (!pesoStr.isEmpty() && !alturaStr.isEmpty()) {
+                    double peso = Double.parseDouble(pesoStr);
+                    double altura = Double.parseDouble(alturaStr);
+                    double imc = peso / (altura * altura);
+
+                    Intent intent = new Intent(MainActivity.this, MainActivityB.class);
+                    intent.putExtra("imc", imc);
+                    startActivity(intent);
+                }
+            }
         });
     }
-
 }
-
 
 
 
