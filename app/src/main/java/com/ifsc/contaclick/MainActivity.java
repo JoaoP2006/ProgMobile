6a2c891fc67b4df5eb4e45186eafcc72a10992a7
joaoP2006 <joao.pv09@aluno.ifsc.edu.br>
2025-05-05 08:32:41 -0300

    ListView

--- app/src/main/java/com/ifsc/contaclick/MainActivity.java
+++ app/src/main/java/com/ifsc/contaclick/MainActivity.java
@@ -4,24 +4,38 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.lang.reflect.Array;
 import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
 
     Integer i = 0;
+    String [] nomes = new String[]{"Anne","Fernanda","joão1", "joão2", "joão3"};
+    ListView lv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ImageView imageView = findViewById(R.id.imageView);
+
+        lv = findViewById(R.id.listview);
+
+        ArrayAdapter<String> a=new ArrayAdapter(this,
+                R.layout.item_lista,
+                R.id.textVieww
+                ,nomes);
+
+        lv.setAdapter(a);
+
 
 
     }
