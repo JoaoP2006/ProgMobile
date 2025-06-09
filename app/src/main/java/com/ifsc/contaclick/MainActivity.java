da144e97fa1f5427961ac72ec4cc7638e690046a
joaoP2006 <joao.pv09@aluno.ifsc.edu.br>
2025-06-09 09:08:53 -0300

    SENSORES

--- app/src/main/java/com/ifsc/contaclick/MainActivity.java
+++ app/src/main/java/com/ifsc/contaclick/MainActivity.java
@@ -1,103 +1,46 @@
 package com.ifsc.contaclick;
 
-import android.content.ContentValues;
-import android.content.Intent;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.ArrayList;
-
-public class MainActivity extends AppCompatActivity {
-
-    SQLiteDatabase db;
-    Button buttonInsere;
-    EditText editText;
-    ListView listView;
-
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
+    int i=0;
+    SensorManager mSensorManager;
+    Sensor sensor;
+    TextView tx,ty,tz;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tx =findViewById(R.id.textViewX);
+        ty =findViewById(R.id.textViewY);
+        tz =findViewById(R.id.textViewZ);
 
-        db = openOrCreateDatabase("banco", MODE_PRIVATE, null);
-        db.execSQL("CREATE TABLE IF NOT EXISTS notas (id INTEGER PRIMARY KEY AUTOINCREMENT, txt VARCHAR)");
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
-        buttonInsere = findViewById(R.id.Button);
-        editText = findViewById(R.id.editTextText);
-        listView = findViewById(R.id.listView);
+//        sensor=mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        sensor=mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
-        buttonInsere.setOnClickListener(v -> {
-            String msg = editText.getText().toString();
-            insereNota(msg);
-            listagemNotas();
-        });
-
-        listagemNotas();
-    }
-
-    public void listagemNotas() {
-        Cursor cursor = db.rawQuery("SELECT * FROM notas", null);
-        cursor.moveToFirst();
-        ArrayList<String> listaNotas = new ArrayList<>();
-
-        while (!cursor.isAfterLast()) {
-            int coluna = cursor.getColumnIndexOrThrow("txt");
-            listaNotas.add(cursor.getString(coluna));
-            cursor.moveToNext();
-        }
-
-        cursor.close();
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listaNotas);
-        listView.setAdapter(adapter);
-
-        listView.setOnItemClickListener((parent, view, position, id) -> {
-            Cursor c = db.rawQuery("SELECT * FROM notas", null);
-            if (c.moveToPosition(position)) {
-                int idxId = c.getColumnIndexOrThrow("id");
-                int idxTxt = c.getColumnIndexOrThrow("txt");
-
-                int notaId = c.getInt(idxId);
-                String textoNota = c.getString(idxTxt);
-
-                Intent intent = new Intent(MainActivity.this, MainActivityB.class);
-                intent.putExtra("id", notaId);
-                intent.putExtra("texto", textoNota);
-                startActivity(intent);
-            }
-            c.close();
-        });
-
-        listView.setOnItemLongClickListener((parent, view, position, id) -> {
-            Cursor c = db.rawQuery("SELECT * FROM notas", null);
-            if (c.moveToPosition(position)) {
-                int idxId = c.getColumnIndexOrThrow("id");
-                int notaId = c.getInt(idxId);
-
-                db.delete("notas", "id = ?", new String[]{String.valueOf(notaId)});
-                listagemNotas();
-            }
-            c.close();
-            return true;
-        });
-    }
-
-    public void insereNota(String txt) {
-        ContentValues cv = new ContentValues();
-        cv.put("txt", txt);
-        db.insert("notas", null, cv);
+        mSensorManager.registerListener(this,sensor,SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        listagemNotas();
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        tx.setText(Float.toString(sensorEvent.values[0]));
+        ty.setText(Float.toString(sensorEvent.values[1]));
+        tz.setText(Float.toString(sensorEvent.values[2]));
+
     }
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
 }
