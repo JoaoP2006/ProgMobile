2acb0351c5247ad0b9753723cce998cc9bf43503
joaoP2006 <joao.pv09@aluno.ifsc.edu.br>
2025-06-09 07:56:31 -0300

    sqlite completo

--- app/src/main/java/com/ifsc/contaclick/MainActivity.java
+++ app/src/main/java/com/ifsc/contaclick/MainActivity.java
@@ -5,75 +5,99 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.lang.reflect.Array;
-import java.text.DecimalFormat;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    Integer i = 0;
     SQLiteDatabase db;
-    EditText editText;
-
-
     Button buttonInsere;
+    EditText editText;
     ListView listView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        db=openOrCreateDatabase("banco",MODE_PRIVATE,null);
 
-        db.execSQL("CREATE TABLE notas (id INTEGER PRIMARY KEY AUTOINCREMENT,txt TEXT)");
-        buttonInsere=findViewById(R.id.Button);
-        editText=findViewById(R.id.editTextText);
-        listView=findViewById(R.id.listView);
-        buttonInsere.setOnClickListener (v -> {
+        db = openOrCreateDatabase("banco", MODE_PRIVATE, null);
+        db.execSQL("CREATE TABLE IF NOT EXISTS notas (id INTEGER PRIMARY KEY AUTOINCREMENT, txt VARCHAR)");
+
+        buttonInsere = findViewById(R.id.Button);
+        editText = findViewById(R.id.editTextText);
+        listView = findViewById(R.id.listView);
+
+        buttonInsere.setOnClickListener(v -> {
             String msg = editText.getText().toString();
             insereNota(msg);
+            listagemNotas();
+        });
+
+        listagemNotas();
+    }
+
+    public void listagemNotas() {
+        Cursor cursor = db.rawQuery("SELECT * FROM notas", null);
+        cursor.moveToFirst();
+        ArrayList<String> listaNotas = new ArrayList<>();
+
+        while (!cursor.isAfterLast()) {
+            int coluna = cursor.getColumnIndexOrThrow("txt");
+            listaNotas.add(cursor.getString(coluna));
+            cursor.moveToNext();
+        }
+
+        cursor.close();
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listaNotas);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener((parent, view, position, id) -> {
+            Cursor c = db.rawQuery("SELECT * FROM notas", null);
+            if (c.moveToPosition(position)) {
+                int idxId = c.getColumnIndexOrThrow("id");
+                int idxTxt = c.getColumnIndexOrThrow("txt");
+
+                int notaId = c.getInt(idxId);
+                String textoNota = c.getString(idxTxt);
+
+                Intent intent = new Intent(MainActivity.this, MainActivityB.class);
+                intent.putExtra("id", notaId);
+                intent.putExtra("texto", textoNota);
+                startActivity(intent);
+            }
+            c.close();
+        });
+
+        listView.setOnItemLongClickListener((parent, view, position, id) -> {
+            Cursor c = db.rawQuery("SELECT * FROM notas", null);
+            if (c.moveToPosition(position)) {
+                int idxId = c.getColumnIndexOrThrow("id");
+                int notaId = c.getInt(idxId);
+
+                db.delete("notas", "id = ?", new String[]{String.valueOf(notaId)});
+                listagemNotas();
+            }
+            c.close();
+            return true;
         });
     }
-       public void listagemNotas(){
 
-           Cursor cursor = db.rawQuery("SELECT * FROM notas", null);
-           cursor.moveToFirst();
-           while (!cursor.isAfterLast()) {
-               int coluna = cursor.getColumnIndex('txt');
-               ListaNotas.add(cursor.getString(coluna));
-               cursor.moveToNext();
-           }
-           ArrayAdapter<String> adapter= new ArrayAdapter<String>(this,
-                   android.R.layout.simple_list_item_1, android.R.id.text1,listaNotas);
-           ListView.setAdapter(adapter);
+    public void insereNota(String txt) {
+        ContentValues cv = new ContentValues();
+        cv.put("txt", txt);
+        db.insert("notas", null, cv);
+    }
 
-
-
-           }
-
-
-
-
-
-
-
-    public void insereNota (String txt){
-        ContentValues cv = new  ContentValues();
-        cv.put("txt",txt);
-     db.insert("notas", null , cv);
+    @Override
+    protected void onResume() {
+        super.onResume();
+        listagemNotas();
     }
 }
-
-
-
-
-
-
--- app/src/main/java/com/ifsc/contaclick/MainActivityB.java
+++ app/src/main/java/com/ifsc/contaclick/MainActivityB.java
@@ -1,25 +1,51 @@
 package com.ifsc.contaclick;
 
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.EditText;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivityB extends AppCompatActivity {
+
+    EditText editText;
+    Button buttonSalvar, buttonCancelar, buttonExcluir;
+    SQLiteDatabase db;
+    int notaId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_b);
-        Bundle budle=getIntent().getExtras();
-        String msg=budle.getString("mensagem");
-        TextView textView = findViewById(R.id.textView);
-        textView.setText(msg);
 
+        editText = findViewById(R.id.editTextNota);
+        buttonSalvar = findViewById(R.id.button5);
+        buttonCancelar = findViewById(R.id.button4);
+        buttonExcluir = findViewById(R.id.button6);
 
+        db = openOrCreateDatabase("banco", MODE_PRIVATE, null);
+
+        Intent intent = getIntent();
+        notaId = intent.getIntExtra("id", -1);
+        String texto = intent.getStringExtra("texto");
+        editText.setText(texto);
+
+        buttonSalvar.setOnClickListener(v -> {
+            String novoTexto = editText.getText().toString();
+            ContentValues cv = new ContentValues();
+            cv.put("txt", novoTexto);
+            db.update("notas", cv, "id = ?", new String[]{String.valueOf(notaId)});
+            finish();
+        });
+
+        buttonExcluir.setOnClickListener(v -> {
+            db.delete("notas", "id = ?", new String[]{String.valueOf(notaId)});
+            finish();
+        });
+
+        buttonCancelar.setOnClickListener(v -> finish());
     }
 }
